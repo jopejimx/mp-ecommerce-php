a07a5386d6a57d9c436719db655da95d8eb83a76
José Pérez <jopeji@gmail.com>
2021-09-22 19:07:10 -0500

    error track

--- detail.php
+++ detail.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 require __DIR__ . '/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 $preference = new MercadoPago\Preference();
